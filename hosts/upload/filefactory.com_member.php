c2c25cd5f5fe3e49a9b8842e8ee7babdb7042e61
totalleecher <60807224+totalleecher@users.noreply.github.com>
2020-05-14 23:32:44 +0630

    Add files via upload

--- hosts/upload/filefactory.com_member.php
+++ hosts/upload/filefactory.com_member.php
@@ -1,87 +1,96 @@
 <?php
+######## Account Info ########
+$upload_acc['filefactory_com']['user'] = ''; //Set your email
+$upload_acc['filefactory_com']['pass'] = ''; //Set your password
+########################
 
-//Input your FileFactory username (email) and password
-$site_login = '';
-$site_pass = '';
+$_GET['proxy'] = isset($_GET['proxy']) ? $_GET['proxy'] : '';
+$not_done = true;
 
+if (!empty($upload_acc['filefactory_com']['user']) && !empty($upload_acc['filefactory_com']['pass'])) {
+	$default_acc = true;
+	$_REQUEST['up_login'] = $upload_acc['filefactory_com']['user'];
+	$_REQUEST['up_pass'] = $upload_acc['filefactory_com']['pass'];
+	$_REQUEST['action'] = '_T8_';
+	echo "<b><center>Using Default Login.</center></b>\n";
+} else $default_acc = false;
 
- /////////////////////////////////////////////////
-$not_done=true;
-$continue_up=false;
-if ($site_login & $site_pass)
-{
-	$_REQUEST['my_login'] = $site_login;
-	$_REQUEST['my_pass'] = $site_pass;
-	$_REQUEST['action'] = "FORM";
-	echo "<center><b>Use Default login/pass...</b></center>\n";
-}
-if ($_REQUEST['action'] == "FORM")
-{
-	$continue_up=true;
-}
-else
-{
-	echo <<<EOF
-<div id=login width=100% align=center>Login to Site</div>
-<table border=0 style="width:350px;" cellspacing=0 align=center>
-	<form method=post>
-		<input type=hidden name=action value='FORM' />
-		<tr><td nowrap>&nbsp;Username*</td><td>&nbsp;<input type=text name=my_login value='' style="width:160px;" />&nbsp;</td></tr>
-		<tr><td nowrap>&nbsp;Password*</td><td>&nbsp;<input type=password name=my_pass value='' style="width:160px;" />&nbsp;</td></tr>
-		<tr><td colspan=2 align=center><input type=submit value='Upload'></td></tr>
-	</form>
-</table>
-EOF;
-}
+if (empty($_REQUEST['action']) || $_REQUEST['action'] != '_T8_') {
+	echo "<table border='0' style='width:270px;' cellspacing='0' align='center'>
+	<form method='POST'>
+	<input type='hidden' name='action' value='_T8_' />
+	<tr><td style='white-space:nowrap;'>&nbsp;EMail*</td><td>&nbsp;<input type='text' name='up_login' value='' style='width:160px;' /></td></tr>
+	<tr><td style='white-space:nowrap;'>&nbsp;Password*</td><td>&nbsp;<input type='password' name='up_pass' value='' style='width:160px;' /></td></tr>\n";
+	echo "<tr><td colspan='2' align='center'><br /><input type='submit' value='Upload' /></td></tr>\n";
+	echo "<tr><td colspan='2' align='center'><small>*You can set it as default in <b>".basename(__FILE__)."</b></small></td></tr>\n";
+	echo "</form>\n</table>\n";
+} else {
+	$login = $not_done = false;
+	$domain = 'www.filefactory.com';
+	$referer = "http://$domain/";
 
-if ($continue_up)
-{
-	$not_done = false;
+	// Login
+	echo "<table style='width:600px;margin:auto;'>\n<tr><td align='center'>\n<div id='login' width='100%' align='center'>Login to $domain</div>\n";
 
-	if ( empty($_REQUEST['my_login']) || empty($_REQUEST['my_pass']) ) html_error('No user and pass given', 0);
-?>
-	<center>
-<?
+	$cookie = array('ff_locale' => 'en_US.utf8');
+	if (!empty($_REQUEST['up_login']) && !empty($_REQUEST['up_pass'])) {
+		if (!empty($_REQUEST['A_encrypted'])) {
+			$_REQUEST['up_login'] = decrypt(urldecode($_REQUEST['up_login']));
+			$_REQUEST['up_pass'] = decrypt(urldecode($_REQUEST['up_pass']));
+			unset($_REQUEST['A_encrypted']);
+		}
+
+		$post = array();
+		$post['loginEmail'] = urlencode($_REQUEST['up_login']);
+		$post['loginPassword'] = urlencode($_REQUEST['up_pass']);
+		$post['Submit'] = 'Sign+In';
+		$page = geturl($domain, 80, '/member/signin.php', "$referer/member/signin.php", $cookie, $post, 0, $_GET['proxy'], $pauth);is_page($page);
+
+		is_present($page, 'The Email Address submitted was invalid', 'Login Failed: Invalid email address.');
+		is_present($page, 'The email address or password you have entered is incorrect.', 'Login Failed: The Email/Password you have entered is incorrect.');
+
+		$cookie = GetCookiesArr($page, $cookie);
+		if (empty($cookie['auth'])) html_error('Login Failed, auth cookie not found.');
+
+		// Check account messages
+		$page = geturl($domain, 80, '/account/', "$referer/member/signin.php", $cookie, 0, 0, $_GET['proxy'], $pauth);is_page($page);
+		is_present($page, "\nLocation: /member/settos.php", 'TOS have changed and need to be approved at the site.');
+		is_present($page, "\nLocation: /member/setpwd.php", 'Your password has expired, please change it.');
+
+		$login = true;
+	} else html_error('Login failed: User/Password empty.');
+
+	// Retrieve upload ID
+	echo "<script type='text/javascript'>document.getElementById('login').style.display='none';</script>\n<div id='info' width='100%' align='center'>Retrieving upload ID</div>\n";
+
 	$post = array();
-	$post['email'] = trim($_REQUEST['my_login']);
-	$post['password'] = trim($_REQUEST['my_pass']);
-	$post['redirect'] = '/';
-	$page = geturl("www.filefactory.com", 80, "/", 0, 0, $post, 0, $_GET["proxy"], $pauth);
-	is_page($page);
-	$cookie = GetCookies($page);
-	if (!preg_match('%ff_membership=%', $cookie)) html_error('Error getting login-cookie - are your logins correct?');
-	$page = geturl("www.filefactory.com", 80, "/?login=1", 0, $cookie, 0, 0, $_GET["proxy"], $pauth);
-	is_page($page);
-	is_notpresent($page, 'You have been signed in as', 'Error logging in - are your logins correct?');
-	?>
-	<script type="text/javascript" language="javascript">document.getElementById('login').style.display='none';</script>
-	<div id=info width="100%" align=center>Retrive upload ID</div>
-	<?php
-	$upload_form = cut_str($page, '<form accept-charset="UTF-8" id="uploader" action="', '"');
-	if (!$url = parse_url($upload_form)) html_error('Error getting upload url');
-	?>
-	<script type="text/javascript" language="javascript">document.getElementById('info').style.display='none';</script>
-	<?php
-	$fpost = array();
-	$fpost['Filename'] = $lname;
-	$fpost['cookie'] = urldecode(str_replace('ff_membership=', '', $cookie));
-	$fpost['folderViewhash'] = '0';
-	$fpost['Upload'] = 'Submit Query';
-	$upfiles = upfile($url["host"],$url["port"] ? $url["port"] : 80, $url["path"].($url["query"] ? "?".$url["query"] : ""), $upload_form, 0, $fpost, $lfile, $lname, "Filedata");
-	?>
-	<script type="text/javascript" language="javascript">document.getElementById('progressblock').style.display='none';</script>
-	<?php
+	$post['cookie'] = rawurldecode($cookie['auth']);
+
+	$up_loc = 'http://upload.filefactory.com/upload';
+	$up_loc .= '-beta.php'; // Old Url
+
+	// Uploading
+	echo "<script type='text/javascript'>document.getElementById('info').style.display='none';</script>\n";
+
+	$url = parse_url($up_loc);
+	$upfiles = upfile($url['host'], defport($url), $url['path'] . (!empty($url['query']) ? '?' . $url['query'] : ''), "$referer/upload/", 0, $post, $lfile, $lname, 'Filedata', '', $_GET['proxy'], $pauth, 0, $url['scheme']);
+
+	// Upload Finished
+	echo "<script type='text/javascript'>document.getElementById('progressblock').style.display='none';</script>";
+
 	is_page($upfiles);
-	if (!preg_match('%\r\n\r\n([a-z0-9]{7})$%', $upfiles, $curi)) html_error('Couldn\'t get the download link, but the file might have been uploaded to your account ok');
-	$completeurl = 'http://www.filefactory.com/file/complete.php/' . $curi[1] . '/';
-	$Url = parse_url($completeurl);
-	$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"].($Url["query"] ? "?".$Url["query"] : ""), 0, $lcook[1], 0, 0, $_GET["proxy"], $pauth);
-	is_page($page);
-	is_notpresent($page, 'Upload Complete', 'Error getting download link - The upload probably failed');
-	$download_link = trim(cut_str($page, '<div class="metadata">', '</div>'));
+
+	if (!preg_match('@\s(\w+)\s*$@i', $upfiles, $uid)) html_error('Upload ID not found.');
+	$page = geturl($domain, 80, '/upload/results.php?files='.$uid[1], "$referer/upload/", $cookie, 0, 0, $_GET['proxy'], $pauth);is_page($page);
+
+	if (preg_match('@(?<=/)file/\w+(/[^\r\n\"\'<>\s\t]+)?@i', $page, $dl)) {
+		$download_link = $referer.$dl[0];
+	} else html_error("Download link not found. (ID: {$uid[1]})");
 }
-	?>
-	</center>
-<?php
-//Updated by szalinski 12-Aug-2009
+
+//[17-6-2011]  Written by Th3-822.
+//[15-9-2013]  Rewritten and fixed for new FF site & Removed anon user support. - Th3-822
+//[24-10-2013] Added a error at login. - Th3-822
+//[02-12-2016] Fixed login error msgs & revised plugin. - Th3-822
+
 ?>
